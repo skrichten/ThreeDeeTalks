01ab52ea5a285f6a9efda34ea6482f1762c81550
skrichten <steve@elegantseagulls.com>
2019-06-08 16:01:07 -0400

    some comments.

--- src/pages/CheatStart.js
+++ src/pages/CheatStart.js
@@ -4,8 +4,13 @@ import GhostMesh from '../components/GhostMesh';
 
 const CheatDemo = () => {
 
+  // Allows us to reference the 3D object as "ghost.current"
   const ghost = useRef();
 
+  /**
+   * This hook, provided by react-three-fiber allows us to run code
+   * in the internal render loop (requestAnimationFrame)
+   */
   useRender(() => {
 
   })
