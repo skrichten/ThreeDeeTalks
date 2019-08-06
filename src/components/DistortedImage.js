57ba8d54de22e1067c0ffdf53f147428025a591a
skrichten <steve@elegantseagulls.com>
2019-08-06 13:06:33 -0400

    New distortion approach

--- src/components/DistortedImage.js
+++ src/components/DistortedImage.js
@@ -1,38 +1,46 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { useSpring, animated as a } from 'react-spring';
 
-const config = {
-  mass: 1, tension: 3, friction: 0, precision: 0.01,
-};
 
 const Distorted = styled.img`
   filter: url(#wavedfilter);
 `;
 
-function DistortedImage({ strength = 70 }) {
+function DistortedImage({ strength = 30 }) {
 
   const AnimFeTurbulence = a('feTurbulence');
-  const AnimFeDisplacementMap = a('feDisplacementMap');
-  const AnimFeGaussianBlur = a('feGaussianBlur');
 
-  const from = { freq:'0.002, 0.007', disp: 0, blur: 0 };
-  const to = { freq:'0.003, 0.009', disp: strength, blur: .5 };
-  const {freq, disp, blur} = useSpring( { from, to, config } );
+ const interp = i => r => `
+  ${ 0.006 + (0.004 * Math.cos(r + ( 2 * Math.PI) )) },
+  ${ 0.006 + (0.004 * Math.sin(r + ( 2 * Math.PI) )) }
+`
+
+ const { radians } = useSpring({
+    to: async next => {
+      while (1) await next({ radians: 2 * Math.PI })
+    },
+    from: { radians: 0 },
+    config: { duration: 10000 },
+    reset: true,
+  });
 
   return (
     <React.Fragment>
     <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" >
       <defs>
         <filter id="wavedfilter">
-          <AnimFeTurbulence type="turbulence" baseFrequency={freq}
+          <AnimFeTurbulence
+            baseFrequency={
+              radians.interpolate(interp(1))
+            }
             numOctaves="1" type="fractalNoise" result="turbulence"/>
 
-          <AnimFeDisplacementMap in="SourceGraphic" in2="turbulence"
-            scale={disp} xChannelSelector="R" yChannelSelector="R"
+          <feDisplacementMap in="SourceGraphic" in2="turbulence"
+            scale={strength} xChannelSelector="R" yChannelSelector="R"
             result="distored"
           />
-          <AnimFeGaussianBlur in="distored" stdDeviation={blur} />
+          <feGaussianBlur in="distored" stdDeviation=".5" />
         </filter>
       </defs>
     </svg>
