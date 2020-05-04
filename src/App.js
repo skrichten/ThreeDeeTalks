3dc03d48b161188fd606367d71e8d0fd91360c32
skrichten <steve@elegantseagulls.com>
2020-05-04 11:29:00 -0400

    Basic scroll restoration. Also fixes a bug for crumple work section.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import routes from './routes';
+import ScrollToTop from './components/ScrollToTop';
 import MainNav from './components/MainNav';
 
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App">
         {routes.map((route, i) => (
           <Route key={i} {...route} />
